fc1a2f09ed1f6c76cf22d423b6b2ac24577c8f08
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 12:03:07 +0700

    fix(server): refine config build progress

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using server.Data;
+using server.Repositories;
+using server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,12 +9,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("MySQL"),
     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySQL")))
 );
+builder.Services.AddScoped<ITaskRepository, TaskService>();
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
