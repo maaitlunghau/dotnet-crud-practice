1301dcc80aed682142c2270c65a9ca84a9055c24
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 11:58:31 +0700

    feat(server): configure connection string to MySQL

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,6 +8,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("MySQL"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySQL")))
+);
 
 if (app.Environment.IsDevelopment())
 {
