c66381aeb3cbad2770c91e4174ffd86f79872004
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 14:05:54 +0700

    feat(client): configure HttpClient to connect api

--- client/Program.cs
+++ client/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpClient("api", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5085/api/");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
