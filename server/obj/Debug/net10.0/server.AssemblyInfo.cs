e4244df7ddc6773519f4dc3dcfdcdf4788a56b0b
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 11:45:58 +0700

    chore: configure swagger UI for testing API

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ac7adcf7619d9f05aecf4b6d4d1e79169b279d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dec8decbcec376dd3a5edd7e8f662f7fffb9839")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
