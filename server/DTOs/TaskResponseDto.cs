9897fda84e5a87d6095a17d96e101cf6ad365e69
maaitlunghau <chunhau.py@gmail.com>
2026-04-02 13:50:57 +0700

    feat(server): add DTO class for task

---
+++ server/DTOs/TaskResponseDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using server.Enums;
+
+namespace server.DTOs;
+
+public class TaskResponseDto
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public PriorityTask Priority { get; set; }
+
+    public StatusTask Status { get; set; } = StatusTask.Pending;
+
+    public DateTime DueDate { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
